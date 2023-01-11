361c1c51814530daaf0b1187b82a1ed8cd9a3747
ClaudiaGambi <117156610+ClaudiaGambi@users.noreply.github.com>
2023-01-11 16:25:50 +0100

    Update board.py

--- board.py
+++ board.py
@@ -4,12 +4,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 class Cars():
+    # create the rectangle for a single car
     def __init__(self, x_coord, y_coord, length):
         self.x = x_coord
         self.y = y_coord
         plt.Rectangle((self.x, self.y), 1, length)
 
 class Board():
+    # create grid with matplotlib
+    
     def __init__(self, column = 6, row = 6):
         self.column = column
         self.row = row
@@ -19,6 +22,8 @@ class Board():
 
 
     def create_board(self):
+        # draw the grid
+        
         for x in range(self.column + 1):
             self.ax.plot([x, x], [0, self.column], 'k')
         for y in range(self.row + 1):
@@ -33,16 +38,18 @@ class Board():
         plt.show()
 
     def create_car(self):
+        
+        # get car info from csv and turn into df
         df = pd.read_csv('gameboards/Rushhour6x6_1.csv')
         print(df)
+        
+        # loop over dataframe and append each car in a list
         for row, column in df.iterrows():
             col = column[2]
             print(col)
             row = column[3]
             length = column[4]
             self.cars_list.append(Cars(col,row, length))
-
-
 
 Board = Board()
 # Board.create_board()
