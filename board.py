ebb0c269e8d62605d8747b68601d0af0a13cfb14
sveta-crypto <117098352+sveta-crypto@users.noreply.github.com>
2023-01-11 16:26:25 +0100

    Add files via upload

--- board.py
+++ board.py
@@ -4,15 +4,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 class Cars():
-    # create the rectangle for a single car
     def __init__(self, x_coord, y_coord, length):
         self.x = x_coord
         self.y = y_coord
         plt.Rectangle((self.x, self.y), 1, length)
 
 class Board():
-    # create grid with matplotlib
-    
     def __init__(self, column = 6, row = 6):
         self.column = column
         self.row = row
@@ -22,8 +19,6 @@ class Board():
 
 
     def create_board(self):
-        # draw the grid
-        
         for x in range(self.column + 1):
             self.ax.plot([x, x], [0, self.column], 'k')
         for y in range(self.row + 1):
@@ -35,15 +30,15 @@ class Board():
 
         self.ax.set_xlim(-1, self.column + 1)
         self.ax.set_ylim(-1, self.row + 1)
+
+        for car in self.cars_list:
+            self.ax.add_patch(car)
+
         plt.show()
 
     def create_car(self):
-        
-        # get car info from csv and turn into df
         df = pd.read_csv('gameboards/Rushhour6x6_1.csv')
         print(df)
-        
-        # loop over dataframe and append each car in a list
         for row, column in df.iterrows():
             col = column[2]
             print(col)
@@ -51,6 +46,8 @@ class Board():
             length = column[4]
             self.cars_list.append(Cars(col,row, length))
 
+
+
 Board = Board()
-# Board.create_board()
+Board.create_board()
 Board.create_car()
