eddf4a9b41a4e6a69b63658509bf373031f174c0
ClaudiaGambi <117156610+ClaudiaGambi@users.noreply.github.com>
2023-01-12 13:05:54 +0100

    Update board.py

--- board.py
+++ board.py
@@ -5,7 +5,10 @@ import pandas as pd
 import random
 
 class Cars():
-
+    '''
+    This class transforms the values for each car into a class object.
+    These values include coordinates, size , type and orientation of the car.
+    '''
     def __init__(self, x_coord, y_coord, length, orientation, type):
         self.x = x_coord - 1
         self.y = y_coord - 1
@@ -13,18 +16,19 @@ class Cars():
         self.orientation = orientation
         self.type = type
 
-
-
-
 class Board():
+    '''
+    The board class creates a grid that will function as the playing board.
+    The default size of the board is 6x6, like it is for the first game.
+    '''
     def __init__(self, column = 6, row = 6):
         self.column = column
         self.row = row
         self.fig = plt.figure(figsize = [self.column, self.row])
         self.ax = self.fig.add_subplot(111)
 
-
     def create_board(self):
+        # the k is for the lines!!!
         for x in range(self.column + 1):
             self.ax.plot([x, x], [0, self.column], 'k')
         for y in range(self.row + 1):
@@ -37,86 +41,53 @@ class Board():
         self.ax.set_xlim(-1, self.column + 1)
         self.ax.set_ylim(-1, self.row + 1)
 
-        # plt.show()
+'''
+Alles hieronder beter nog in class/objects/functie oid voor overzicht.
+Dat werkte eerst niet want we kregen de error dat de Board class referenced
+before assignment was.
+'''
 
-    # def create_car(self):
-        # df = pd.read_csv('gameboards/Rushhour6x6_1.csv')
-        # # print(df)
-        #
-        #
-        # for row, column in df.iterrows():
-        #     col = column[2]
-        #     # print(col)
-        #     row = column[3]
-        #     length = column[4]
-        #     orientation = column[1]
-        #     # print(orientation)
-        #
-        #     self.cars_list.append(Cars(col, row, length, orientation))
-        # print(len(self.cars_list))
 
-# class RushHour():
-#     def __init__(self):
-#         self.cars_list = []
-#
-#
-#
-#     def create_car(self):
-#         df = pd.read_csv('gameboards/Rushhour6x6_1.csv')
-#         # print(df)
-#
-#         for row, column in df.iterrows():
-#             col = column[2]
-#             # print(col)
-#             row = column[3]
-#             length = column[4]
-#             orientation = column[1]
-#             # print(orientation)
-#             self.cars_list.append(Cars(col, row, length, orientation))
-#         print(len(self.cars_list))
-#
-#
-#     def board(self):
-#         Board = Board()
-#         Board.create_board()
-#
-#         for car in self.cars_list:
-#             print(car)
-#             print(car.orientation)
-#             if car.orientation == 'H':
-#                 Board.ax.add_patch(plt.Rectangle(car.x, car.y), 1, car.length)
-#                 print('hi')
-#             else:
-#                 Board.ax.add_patch(plt.Rectangle(car.x, car.y), car.length, 1)
-#
-
-        # Board.plt.show()
-
-# game = RushHour()
-# game.create_car()
 Board = Board()
 Board.create_board()
-# Board.create_car()
 
 cars_list = []
-df = pd.read_csv('gameboards/Rushhour6x6_1.csv')
+
+# load the data from the game into dataframe
+
+# filenames als input in terminal geven
+filename = 'Rushhour6x6_1.csv'
+df = pd.read_csv(filename)
 print(df)
+
+# loop over the rows in the df and select the right values
 for row, column in df.iterrows():
     col = column[2]
     row = column[3]
     length = column[4]
     orientation = column[1]
     type = column[0]
+
+    # for each row (each car), make it into a Cars class and append this to the cars list
     cars_list.append(Cars(col, row, length, orientation, type))
 
+# for each car in the car list, plot them in the board grid
 for car in cars_list:
 
+    # if the car moves horizontally, plot car in a rondom color
     if car.orientation == 'H':
         Board.ax.add_patch(mpatches.Rectangle((car.x, car.y), car.length, 1, facecolor = (random.random(), random.random(), random.random())))
-        if car.orientation == 'H' and car.type == 'X':
+
+        # if car is type X, make the color red
+        if car.type == 'X':
             Board.ax.add_patch(mpatches.Rectangle((car.x, car.y), car.length, 1, facecolor = 'r' ))
 
+    # if the car moves vertically, plot car in random color
     else:
         Board.ax.add_patch(mpatches.Rectangle((car.x, car.y), 1, car.length, facecolor = (random.random(), random.random(), random.random())))
+
+# flip the board so it matches the game picture
 plt.gca().invert_yaxis()
+
 plt.show()
+
