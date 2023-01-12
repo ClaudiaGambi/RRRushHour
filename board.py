b277af0b0ba322b8cee919df0dbf4a50b5bf18ee
sveta-crypto <117098352+sveta-crypto@users.noreply.github.com>
2023-01-12 12:46:01 +0100

    Add files via upload

--- board.py
+++ board.py
@@ -2,14 +2,16 @@ import matplotlib.path as mpath
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import pandas as pd
+import random
 
 class Cars():
 
-    def __init__(self, x_coord, y_coord, length, orientation):
-        self.x = x_coord
-        self.y = y_coord
+    def __init__(self, x_coord, y_coord, length, orientation, type):
+        self.x = x_coord - 1
+        self.y = y_coord - 1
         self.length = length
         self.orientation = orientation
+        self.type = type
 
 
 
@@ -35,70 +37,86 @@ class Board():
         self.ax.set_xlim(-1, self.column + 1)
         self.ax.set_ylim(-1, self.row + 1)
 
+        # plt.show()
 
+    # def create_car(self):
+        # df = pd.read_csv('gameboards/Rushhour6x6_1.csv')
+        # # print(df)
+        #
+        #
+        # for row, column in df.iterrows():
+        #     col = column[2]
+        #     # print(col)
+        #     row = column[3]
+        #     length = column[4]
+        #     orientation = column[1]
+        #     # print(orientation)
+        #
+        #     self.cars_list.append(Cars(col, row, length, orientation))
+        # print(len(self.cars_list))
 
-class RushHour():
-    def __init__(self):
-        self.cars_list = []
+# class RushHour():
+#     def __init__(self):
+#         self.cars_list = []
+#
+#
+#
+#     def create_car(self):
+#         df = pd.read_csv('gameboards/Rushhour6x6_1.csv')
+#         # print(df)
+#
+#         for row, column in df.iterrows():
+#             col = column[2]
+#             # print(col)
+#             row = column[3]
+#             length = column[4]
+#             orientation = column[1]
+#             # print(orientation)
+#             self.cars_list.append(Cars(col, row, length, orientation))
+#         print(len(self.cars_list))
+#
+#
+#     def board(self):
+#         Board = Board()
+#         Board.create_board()
+#
+#         for car in self.cars_list:
+#             print(car)
+#             print(car.orientation)
+#             if car.orientation == 'H':
+#                 Board.ax.add_patch(plt.Rectangle(car.x, car.y), 1, car.length)
+#                 print('hi')
+#             else:
+#                 Board.ax.add_patch(plt.Rectangle(car.x, car.y), car.length, 1)
+#
 
+        # Board.plt.show()
 
-
-    def create_car(self):
-        df = pd.read_csv('gameboards/Rushhour6x6_1.csv')
-        # print(df)
-
-        for row, column in df.iterrows():
-            col = column[2]
-            # print(col)
-            row = column[3]
-            length = column[4]
-            orientation = column[1]
-            # print(orientation)
-            self.cars_list.append(Cars(col, row, length, orientation))
-        print(len(self.cars_list))
-
-
-    def board(self):
-        Board = Board()
-        Board.create_board()
-
-        for car in self.cars_list:
-            print(car)
-            print(car.orientation)
-            if car.orientation == 'H':
-                Board.self.ax.add_patch(plt.Rectangle(car.x, car.y), 1, car.length)
-                print('hi')
-            else:
-                Board.self.ax.add_patch(plt.Rectangle(car.x, car.y), car.length, 1)
-
-
-        Board.plt.show()
-
-game = RushHour()
-game.create_car()
-# Board.create_board()
+# game = RushHour()
+# game.create_car()
+Board = Board()
+Board.create_board()
 # Board.create_car()
 
-# cars_list = []
-# df = pd.read_csv('gameboards/Rushhour6x6_1.csv')
-# # print(df)
-# for row, column in df.iterrows():
-#     col = column[2]
-#     # print(col)
-#     row = column[3]
-#     length = column[4]
-#     orientation = column[1]
-#     # print(orientation)
-#     cars_list.append(Cars(col, row, length, orientation))
-# # print(len(self.cars_list))
-#
-# for car in self.cars_list:
-#     print(car)
-#     print(car.orientation)
-#     if car.orientation == 'H':
-#         self.ax.add_patch(plt.Rectangle(car.x, car.y), 1, car.length)
-#         print('hi')
-#     else:
-#         self.ax.add_patch(plt.Rectangle(car.x, car.y), car.length, 1)
-#
-# plt.show()
+cars_list = []
+df = pd.read_csv('gameboards/Rushhour6x6_1.csv')
+print(df)
+for row, column in df.iterrows():
+    col = column[2]
+    row = column[3]
+    length = column[4]
+    orientation = column[1]
+    type = column[0]
+    cars_list.append(Cars(col, row, length, orientation, type))
+
+for car in cars_list:
+
+    if car.orientation == 'H':
+        Board.ax.add_patch(mpatches.Rectangle((car.x, car.y), car.length, 1, facecolor = (random.random(), random.random(), random.random())))
+        if car.orientation == 'H' and car.type == 'X':
+            Board.ax.add_patch(mpatches.Rectangle((car.x, car.y), car.length, 1, facecolor = 'r' ))
+
+    else:
+        Board.ax.add_patch(mpatches.Rectangle((car.x, car.y), 1, car.length, facecolor = (random.random(), random.random(), random.random())))
+plt.gca().invert_yaxis()
+plt.show()
