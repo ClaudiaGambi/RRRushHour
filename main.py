b8ca6c7004bb8b09b7204aa725ed58c0b93b5c0e
Claudia <cgambirasio1@gmail.com>
2023-01-16 11:09:24 +0100

    comments

--- main.py
+++ main.py
@@ -15,11 +15,16 @@ if __name__ == '__main__':
     #While not not solved:
 
         #Propose a random step:
-
-        # !!!!!!!!!!!!!!!!!
+        # choose randomly from car dictionary
         random_car_step = random.choice(Board.cars_dictionary)
+
+        # choose direction randomly
         random_direction_step = random.choice([1, -1])
+
+        # to check order of step, keep track of total number of steps in game
         number_step = 0
+
+        # create step
         step = (random_car_step, number_step += 1, random_direction_step)
 
         new_coordinates = car.step(car, 1)
