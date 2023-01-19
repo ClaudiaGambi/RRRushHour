9e8443cd69bdbca5dfbdba144e21974ad89063c3
Sveta Roopram <svetaroopram@MacBook-Air-van-Sveta.local>
2023-01-19 12:26:56 +0100

    classes geupdate

--- code/classes/plots.py
+++ code/classes/plots.py
@@ -29,42 +29,23 @@ class Plot_board():
         self.ax.set_xlim(-1, self.board_size + 1)
         self.ax.set_ylim(-1, self.board_size + 1)
 
-    def add_cars_in_plot(self, dictionary):
-        """
-        Method that reads coordination information of the cars dictionary. 
-        Creates a plot representing the board based on that information.
-        """
+    
 
-        # for every car in the dictionary
-        for car in dictionary.keys():
-
-            # check the orientation
-            if dictionary[car][2] == 'H':
-                self.ax.add_patch(mpatches.Rectangle(dictionary[car][0][0], dictionary[car][1], 1, facecolor = dictionary[car][3] ))
-
-            # plot patch with different orientation
-            else:
-                self.ax.add_patch(mpatches.Rectangle(dictionary[car][0][0], 1, dictionary[car][1], facecolor = dictionary[car][3] ))
-
-        #Mirror board:
-        plt.gca().invert_yaxis()
-        plt.show()
-
-    def plot_dotted_cars(self, dictionary):
+    def plot_dotted_cars(self, cars_list):
         plt.ion()
         # fig = plt.figure()
         # ax = fig.add_subplot(111)
         #Loop through cars list:
-        for car in dictionary.keys():
+        for car in cars_list:
             #Select the color:
-            color = dictionary[car][3]
+            color = car.color
             # print(f"Car: {car}")
             #Loop through every coordinate of the car:
-            for coordinate in dictionary[car][0]:
-                print(coordinate)
-                column = coordinate[0]
-                row = coordinate[1]
-                self.ax.plot(column - 0.5,row - 0.5, marker = "s", color = color, markersize = 50)
+            
+                
+            column = car.coordinates_list[0]
+            row = car.coordinates_list[1]
+            self.ax.plot(column - 0.5,row - 0.5, marker = "s", color = color, markersize = 50)
         
         #Make sure the board is squared:
         #self.ax.set_position([0, 0, 1, 1])
--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from code.classes import board
+from code.classes  import plots
+from code.algorithms import randomize
 import random
 import pandas as pd
-from code.classes  import plots
-from code.algorithms import random_algo
 
 filename = 'gameboards/Rushhour6x6_1.csv'
 
 if __name__ == '__main__':
 
     #Create starting board:
-    Board = board.Board()
-    Board.read_board_size(filename)
-    
-    # Board.board_in_array()
-    Board.df_to_dict(filename)
+    Board = board.Board(filename)
 
-    cars_dictionary = Board.cars_dict
+     # create object of cars
+    Board.df_to_object()
+
+    
     step_dictionary = Board.step_dict
 
     #Plot situation:
@@ -25,47 +24,6 @@ if __name__ == '__main__':
     plot.plot_dotted_cars(cars_dictionary)
 
     #List to keep track of steps:
-    lst = []
-
-    #While not not solved:
-    while cars_dictionary['X'][0][0] != (5, 4):
-    #for i in range(60):
-        # move count
-        move_count = 0
-
-        # choose randomly from car dictionary
-        random_car = random.choice(list(cars_dictionary.keys()))
-
-        # choose direction randomly
-        random_direction = random.choice([1, -1])
-        
-        # propose a random step:
-        new_coordinates = Board.step(random_car, cars_dictionary, random_direction)
-
-        #Check availablity:
-        availability = Board.check_availability(random_car, new_coordinates, cars_dictionary)
-
-        #If available, save the step in (updated) dictionary:
-        if availability == True:
-            print(f"Old coordinates: {cars_dictionary[random_car][0]}")
-            print(f"New coordinates: {new_coordinates}")
-            cars_dictionary[random_car][0] = new_coordinates
-            move_count += 1
-            # print(cars_dictionary)
-
-            #Plot current situation:
-            plot2 = plots.Plot_board()
-            plot2.create_board()
-            #plot2.add_cars_in_plot(cars_dictionary)
-            plot2.plot_dotted_cars(cars_dictionary)
-        
-        # Keep track of the game steps:
-        lst.append([random_car, random_direction])
-
-            
-    steps_df = pd.DataFrame(lst, columns = ["car", "move"])
-    print(steps_df.head())
-
-        
+    
 
 
