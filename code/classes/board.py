41bee35667f6b3951a94e96d649adcb6f2cec4a0
Claudia <cgambirasio1@gmail.com>
2023-01-19 13:21:59 +0100

    updated comments

--- code/classes/board.py
+++ code/classes/board.py
@@ -6,8 +6,13 @@ import itertools
 
 class Board():
     '''
-    The board class creates a grid that will function as the playing board.
-    The default size of the board is 6x6, like it is for the first game.
+    The Board class has two functions: df_to_object and check_availability.
+    The df_to_object reads the file containing the initial board state and 
+    saves this in a Car object.
+
+    The check availability function checks for every propsed move if this 
+    move can be done: whether another car is in the way or wheter the car 
+    will move of the board.
     '''
     def __init__(self, file_name):
         self.column = int(file_name[19])
@@ -19,6 +24,8 @@ class Board():
 
 
     def df_to_object(self):
+
+        # read the csv file into a dataframe
         df = pd.read_csv(self.file)
 
         for row, column in df.iterrows():
@@ -38,8 +45,11 @@ class Board():
                 
             #Check orientation:
             if orientation == "V":
+
                 #Create list of coordinates that are covered by the car:
+                #loop over the length of the car to append each taken coordinate to the coordinates list
                 for i in range(length):
+
                     #Save coordinate to list:
                     list_coordinates.append(coord)
 
@@ -49,7 +59,9 @@ class Board():
 
             else:
                 #Create list of coordinates that are covered by the car:
+                #loop over the length of the car to append each taken coordinate to the coordinates list
                 for i in range(length):
+
                     #Save coordinate to list:
                     list_coordinates.append(coord)
 
@@ -61,49 +73,43 @@ class Board():
             self.cars_list.append(car.Car(list_coordinates, length, orientation, car_type, color))
 
     def check_availability(self, random_car):
-        """Method that checks whether the proposed coordinates (input) are not taken by another car yet.
+        """
+        Method that checks whether the proposed coordinates (input) are not taken by another car yet.
         It also checks that the car is not going of the board yet. Outputs boolean that is True when 
-        the proposed coordinates are a possibility."""
-
+        the proposed coordinates are a possibility.
+        """
         #Make set of car coordinates:
-        
         set_new_coordinates= set(random_car.updated_coordinates)
 
         #Check whether the car coordinates are not of the board yet:
         flat_coords = list(itertools.chain(*set_new_coordinates))
 
+        #Check if car won't move off the board
         for number in flat_coords:
             if number > self.column or number < 1:
-               
                 return False
 
-        count = 0
         #Loop through cars list:
         for car in self.cars_list:
 
             #Don't check with self:
             if car.type == random_car.type:
-                
                 continue
             
-            #Count cars:
-            count +=1
             #Make a set of the car coordinates:
             coords = set(car.coordinates_list)
             
             #Check whether there is overlap:
             overlap = set_new_coordinates.intersection(coords)
             
-            
             #If there's no overlap, go to next car:
             if len(overlap) < 1:
-                
                 continue
         
             #Overlap? Availibilty not approved:
-            
             return False
         
+        # if there is no overlap and the car stays on the board:
         return True
 
    
--- code/classes/car.py
+++ code/classes/car.py
@@ -3,14 +3,17 @@ import pandas as pd
 
 class Car():
     '''
-    This class transforms the values for each car into a class object.
-    These values include coordinates, size , type and orientation of the car.
-    '''
+    This class initiates a car object. In this object attributes with the coordinates,
+    length of car, orientation, identification, and color are created.
 
+    In this class, the function step is created too. This function lets a car take a step.
+    '''
     def __init__(self, coordinates_list, length, orientation, type, color):
         self.coordinates_list = coordinates_list
         self.length = length
         self.orientation = orientation
+
+        # identification of car
         self.type = type
         self.color = color
         self.updated_coordinates = []
@@ -19,21 +22,29 @@ class Car():
     def step(self, direction = 1):
         '''
         Function to make a car move. The input is the car object and the direction
-        in which it should move (1 or -1). Outputs new coordinates.
+        in which it should move (1 or -1). The function outputs new coordinates.
         '''
-        
-       
+        # if the car moves horizontally, add the direction to the current x coordinate
         if self.orientation == 'H':
+
+            # loop over all coordinates the car is on
             for coordinate in self.coordinates_list:
 
                 updated_x = coordinate[0] + direction
                 updated_coordinate = (updated_x, coordinate[1])
+
+                # update the coordinate list
                 self.updated_coordinates.append(updated_coordinate)
 
+        # if the car moves vertically, add the direction to the current y coordinate
         if self.orientation== 'V':
+
+            # loop over all coordinates the car is on
             for coordinate in self.coordinates_list:
                 updated_y = coordinate[1] + direction
                 updated_coordinate = (coordinate[0], updated_y)
+
+                # update the coordinate list
                 self.updated_coordinates.append(updated_coordinate)
         
         
